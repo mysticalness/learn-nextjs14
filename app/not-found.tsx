5bee71af5afc45d65a5be2cff8f1a4dff1c05602
mysticalness <kih9079@naver.com>
2024-11-09 20:19:03 +0900

    #2.7 Layouts

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,5 @@
-import Navigation from "../components/navigation";
-
 export default function NotFound() {
     return <div>
-        <Navigation />
         <h1>Not Found!</h1>
     </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import Navigation from "../components/navigation"
-
 export default function Page() {
     return <div>
-        <Navigation />
         <h1>Hello!</h1>
     </div>
 }
