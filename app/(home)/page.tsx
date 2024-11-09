de16c6a32dce9ad3d12de620ec3f08451ab22b46
mysticalness <kih9079@naver.com>
2024-11-09 20:40:20 +0900

    route group & metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title : "Home"
+}
+
 export default function Page() {
     return <div>
         <h1>Hello!</h1>
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title : "About us"
+}
+
 export default function AboutUs() {
     return <div>
         <h1>About Us!</h1>
