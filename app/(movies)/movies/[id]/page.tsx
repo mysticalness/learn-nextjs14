e50c1e79e56752c3a4b55cc2ebb9ef8d3e79f969
mysticalness <kih9079@naver.com>
2024-11-11 16:02:35 +0900

    #3.5 Suspense

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,31 +1,21 @@
-import { API_URL } from "../../../(home)/page"
-
-async function getMovie(id: string) {
-    console.log(`Fetching movies: ${Date.now()}`)
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}`)
-    return response.json();
-}
-
-async function getVideos(id: string) {
-    console.log(`Fetching videos: ${Date.now()}`)
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`)
-    return response.json();
-}
+import { Suspense } from "react"
+import MovieInfo from "../../../../components/movie-info"
+import MovieVideos from "../../../../components/movie-video"
 
 export default async function MovieDetail({
     params: { id },
 }: {
     params: { id: string }
 }) {
-    // console.log(`start fectching`)
-    // const movie = await getMovie(id)
-    // const videos = await getVideos(id);
-    // console.log(`end fetching`)
-    
-    console.log("start fetching")
-    const [movie, videos ] = await Promise.all([getMovie(id),getVideos(id)])
-    console.log("end fetching")
-    return <h1>{movie.title}</h1>
+    return (
+        <div>
+            <h3>Movie detail page</h3>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
+    )
 }
