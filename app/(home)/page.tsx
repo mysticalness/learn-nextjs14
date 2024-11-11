825a789c2af48030318d9097e984a638fb5aa713
mysticalness <kih9079@naver.com>
2024-11-11 14:30:29 +0900

    server side, loading component, parallel request

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,25 @@
-"use client"
+import Link from "next/link";
 
-import { useEffect, useState } from "react"
+export const metadata = {
+    title: "Home",
+}
 
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
-export default function Page() {
+async function getMovies() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return json;
+}
 
-    const [isLoading, setIsLoading] = useState(true);
-    const [movies, setMovies] = useState();
-
-    const getMovies = async () => {
-       const response =  await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-       const json = await response.json();
-       setMovies(json);
-    }
-    
-    useEffect(()=> {
-        getMovies();
-        setIsLoading(false)
-    },[])
+export default async function HomePage() {
+    const movies = await getMovies();
     return <div>
-        {isLoading ? "Loading..." : JSON.stringify(movies)}
+        {movies.map((movie)=> 
+            <li key={movie.id}>
+                <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+            </li>
+        )}
     </div>
 }
